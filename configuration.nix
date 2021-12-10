72149db343675684ed5a0f5d6e87f25dfe1a1186
Dan G <dang@delete.run>
2021-12-09 16:23:10 -0800

    hmm

--- configuration.nix
+++ configuration.nix
@@ -145,30 +145,30 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Samba
-  services.samba = {
-   enable = true;
-   securityType = "user";
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = flux-capacitor
-      server role = standalone server
-      netbios name = flux-capacitor
-      security = user
-      ;use sendfile = yes
-      ;max protocol = smb2
-      ;"hosts allow" = 192.168.0 localhost
-      ;"hosts deny" = 0.0.0.0/0
-      "guest account" = nobody
-      "map to guest" = bad user
-    '';
-    shares = {
-      distributed = {
-        path = "/srv/pool";
-        browseable = "yes";
-        public = "no";
-        writable = "yes";
-      };
+  # # Samba
+  # services.samba = {
+  #  enable = true;
+  #  securityType = "user";
+  #   extraConfig = ''
+  #     workgroup = WORKGROUP
+  #     server string = flux-capacitor
+  #     server role = standalone server
+  #     netbios name = flux-capacitor
+  #     security = user
+  #     ;use sendfile = yes
+  #     ;max protocol = smb2
+  #     ;"hosts allow" = 192.168.0 localhost
+  #     ;"hosts deny" = 0.0.0.0/0
+  #     "guest account" = nobody
+  #     "map to guest" = bad user
+  #   '';
+  #   shares = {
+  #     distributed = {
+  #       path = "/srv/pool";
+  #       browseable = "yes";
+  #       public = "no";
+  #       writable = "yes";
+  #     };
 
   # System Packages
   environment.systemPackages = with pkgs; [
