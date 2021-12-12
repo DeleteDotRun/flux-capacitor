3db1f7971a407ca7729d30da5c9a661a8515f982
Dan G <dang@delete.run>
2021-12-12 14:59:57 -0800

    disabled grafana

--- configuration.nix
+++ configuration.nix
@@ -44,9 +44,9 @@
     # 8444 8555 # Chia
     445 139 # Samba
     3389 # XRDP
-    2342 # Grafana
-    9001 9002 # Prometheus
-    28183 3100 # Loki
+    # 2342 # Grafana
+    # 9001 9002 # Prometheus
+    # 28183 3100 # Loki
   ];
   networking.firewall.allowedUDPPorts = [
     137 138 # Samba
@@ -178,60 +178,60 @@
   };
 
 
-  # grafana configuration
-  services.grafana = {
-    enable = true;
-    domain = "grafana.local";
-    port = 2342;
-    addr = "127.0.0.1";
-  };
+  # # grafana configuration
+  # services.grafana = {
+  #   enable = true;
+  #   domain = "grafana.local";
+  #   port = 2342;
+  #   addr = "127.0.0.1";
+  # };
 
-  # Prometheus
-  services.prometheus = {
-    enable = true;
-    port = 9001;
-    exporters = {
-      node = {
-        enable = true;
-        enabledCollectors = [ "systemd" ];
-        port = 9002;
-      };
-    };
-    scrapeConfigs = [
-      {
-        job_name = "flux-capacitor";
-        static_configs = [{
-          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-        }];
-      }
-    ];
-  };
+  # # Prometheus
+  # services.prometheus = {
+  #   enable = true;
+  #   port = 9001;
+  #   exporters = {
+  #     node = {
+  #       enable = true;
+  #       enabledCollectors = [ "systemd" ];
+  #       port = 9002;
+  #     };
+  #   };
+  #   scrapeConfigs = [
+  #     {
+  #       job_name = "flux-capacitor";
+  #       static_configs = [{
+  #         targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+  #       }];
+  #     }
+  #   ];
+  # };
 
-  services.loki = {
-    enable = true;
-    configFile = ./loki.yaml;
-  };
+  # services.loki = {
+  #   enable = true;
+  #   configFile = ./loki.yaml;
+  # };
 
-  systemd.services.promtail = {
-    description = "Promtail service for Loki";
-    wantedBy = [ "multi-user.target" ];
+  # systemd.services.promtail = {
+  #   description = "Promtail service for Loki";
+  #   wantedBy = [ "multi-user.target" ];
 
-    serviceConfig = {
-      ExecStart = ''
-        ${pkgs.grafana-loki}/bin/promtail --config.file ${./promtail.yaml}
-      '';
-    };
-  };
+  #   serviceConfig = {
+  #     ExecStart = ''
+  #       ${pkgs.grafana-loki}/bin/promtail --config.file ${./promtail.yaml}
+  #     '';
+  #   };
+  # };
 
 
 
-  # nginx reverse proxy
-  services.nginx.virtualHosts.${config.services.grafana.domain} = {
-    locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-        proxyWebsockets = true;
-    };
-  };
+  # # nginx reverse proxy
+  # services.nginx.virtualHosts.${config.services.grafana.domain} = {
+  #   locations."/" = {
+  #       proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+  #       proxyWebsockets = true;
+  #   };
+  # };
 
 
 
