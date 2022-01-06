4abad7d5335dba3d68473365f00b5e1db391f96d
Dan G <dang@delete.run>
2022-01-06 02:14:37 -0800

    enable Prometheus

--- configuration.nix
+++ configuration.nix
@@ -198,26 +198,26 @@
     addr = "127.0.0.1";
   };
 
-  # # Prometheus
-  # services.prometheus = {
-  #   enable = true;
-  #   port = 9001;
-  #   exporters = {
-  #     node = {
-  #       enable = true;
-  #       enabledCollectors = [ "systemd" ];
-  #       port = 9002;
-  #     };
-  #   };
-  #   scrapeConfigs = [
-  #     {
-  #       job_name = "flux-capacitor";
-  #       static_configs = [{
-  #         targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
-  #       }];
-  #     }
-  #   ];
-  # };
+  # Prometheus
+  services.prometheus = {
+    enable = true;
+    port = 9001;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+    scrapeConfigs = [
+      {
+        job_name = "flux-capacitor";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+  };
 
   # services.loki = {
   #   enable = true;
